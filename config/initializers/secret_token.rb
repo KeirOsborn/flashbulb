e766d36321a3d071b41d2fa7f854aa594d18efde
Keir Osborn <keir.osborn@gmail.com>
2013-06-24 12:57:42 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashbulb::Application.config.secret_token = '6cbb06a6feb9533414a22bbc4b012a2aaf98f7282084e8d8d9527dbb27747603b3070063da04f4f72263c9e84666eaa158d7a828f9dfb9c9abd93380a7d6624b'
